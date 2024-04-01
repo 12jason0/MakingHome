1271946dbb9198278ddb35cef4720b5e6dbb2b30
gh9727 <gj9727@gmail.com>
2024-04-01 14:44:22 +0900

    1

--- views/src/pages/RegisterPage.tsx
+++ views/src/pages/RegisterPage.tsx
@@ -113,206 +113,170 @@ export default function RegisterPage({ setShowHeader }: RegisterPageProps) {
         </h3>
       </div>
       <hr style={{ marginTop: '-3%' }} />
-      <div
+      {/* <div
         style={{ display: 'flex', justifyContent: 'center', marginTop: '-3%' }}
       >
         <h3>회원가입</h3>
-      </div>
+      </div> */}
       {/* ======================================================================================================*/}
       {/* ==================================Table Form 회원 정보 입력 창========================================= */}
       {/* ====================================================================================================== */}
       <form onSubmit={handleSubmit(onValid, onInValid)} className="form">
-        <table border={1} className="formTable">
-          <colgroup>
-            <col style={{ width: '80px' }}></col>
-            <col style={{ width: 'auto' }}></col>
-          </colgroup>
+        <div className="formBox">
           {/* 이름 */}
-          <tr>
-            <th scope="row">
-              <label>이름</label>
-            </th>
-            <td>
-              <input
-                className="nameForm"
-                type="text"
-                {...register('name', {
-                  required: '이름은 필수 항목입니다.',
-                  validate: {
-                    trimValid: (name: string) => {
-                      if (name?.trim() === '') return '이름을 입력하세요...';
-                      else {
-                        return true;
-                      }
-                    },
+          <div className="inputForm">
+            <input
+              className="userInput userName"
+              type="text"
+              {...register('name', {
+                required: '이름은 필수 항목입니다.',
+                validate: {
+                  trimValid: (name: string) => {
+                    if (name?.trim() === '') return '이름을 입력하세요...';
+                    else {
+                      return true;
+                    }
                   },
-                })}
-                placeholder="이름"
-              />
-              {errors.name && (
-                <div style={{ fontSize: '12px', color: 'red' }}>
-                  이름을 입력하십시오
-                </div>
-              )}
-            </td>
-          </tr>
+                },
+              })}
+              placeholder="이름"
+            />
+            {errors.name && (
+              <span style={{ fontSize: '12px', color: 'red' }}>
+                이름을 입력하십시오
+              </span>
+            )}
+          </div>
           {/* 나이 */}
-          <tr>
-            <th style={{ border: 'none' }}>
-              <label>나이</label>
-            </th>
-            <td>
-              <input
-                type="text"
-                className="ageForm"
-                {...register('age', {
-                  required: '나이는 필수 항목입니다.',
-                  validate: {
-                    ageType: (age: string | number) => {
-                      if (Number.isNaN(Number(age)) === false) {
-                        return true;
-                      } else {
-                        return '숫자를 입력하세요...';
-                      }
-                    },
+          <div className="inputForm">
+            <input
+              type="text"
+              className="userInput userAge"
+              {...register('age', {
+                required: '나이는 필수 항목입니다.',
+                validate: {
+                  ageType: (age: string | number) => {
+                    if (Number.isNaN(Number(age)) === false) {
+                      return true;
+                    } else {
+                      return '숫자를 입력하세요...';
+                    }
                   },
-                })}
-                placeholder="나이"
-              />
-              <br />
-              {errors.age && (
-                <div style={{ fontSize: '12px', color: 'red' }}>
-                  나이를 입력하십시오
-                </div>
-              )}
-            </td>
-          </tr>
+                },
+              })}
+              placeholder="나이"
+            />
+            <br />
+            {errors.age && (
+              <span style={{ fontSize: '12px', color: 'red' }}>
+                나이를 입력하십시오
+              </span>
+            )}
+          </div>
           {/* 이메일 */}
-          <tr>
-            <th>
-              <label>이메일</label>
-            </th>
-            <td>
-              <input
-                type="email"
-                className="emailForm"
-                {...register('email', {
-                  required: false,
-                  validate: {
-                    trimValid: (email: string) => {
-                      if (email?.trim() === '') return '이메일을 입력하세요...';
-                      else return true;
-                    },
+          <div className="inputForm">
+            <input
+              type="email"
+              className="userInput userEmail"
+              {...register('email', {
+                required: false,
+                validate: {
+                  trimValid: (email: string) => {
+                    if (email?.trim() === '') return '이메일을 입력하세요...';
+                    else return true;
                   },
-                  pattern:
-                    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
-                })}
-                placeholder="이메일"
-              />
-              {errors.email && (
-                <div style={{ fontSize: '12px', color: 'red' }}>
-                  이메일을 입력하세요
-                </div>
-              )}
-            </td>
-          </tr>
+                },
+                pattern:
+                  /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
+              })}
+              placeholder="이메일"
+            />
+            {errors.email && (
+              <span style={{ fontSize: '12px', color: 'red' }}>
+                이메일을 입력하세요
+              </span>
+            )}
+          </div>
           {/* 전화번호 */}
-          <tr>
-            <th>
-              <label>전화번호</label>
-            </th>
-            <td>
-              <input
-                type="text"
-                className="phoneForm"
-                {...register('phone', {
-                  required: false,
-                  validate: {
-                    trimValid: (phone: string) => {
-                      if (phone?.trim() === '')
-                        return '이름은 필수 항목입니다....';
-                      else return true;
-                    },
+          <div className="inputForm">
+            <input
+              type="text"
+              className="userInput userPhone"
+              {...register('phone', {
+                required: false,
+                validate: {
+                  trimValid: (phone: string) => {
+                    if (phone?.trim() === '')
+                      return '이름은 필수 항목입니다....';
+                    else return true;
                   },
-                  pattern: /^01(?:0|1|[6-9])(?:\d{3}|\d{4})\d{4}$/,
-                })}
-                placeholder="전화번호"
-              />
-              {errors.phone && (
-                <div style={{ fontSize: '12px', color: 'red' }}>
-                  전화번호를 입력하십시오
-                </div>
-              )}
-            </td>
-          </tr>
+                },
+                pattern: /^01(?:0|1|[6-9])(?:\d{3}|\d{4})\d{4}$/,
+              })}
+              placeholder="전화번호"
+            />
+            {errors.phone && (
+              <span style={{ fontSize: '12px', color: 'red' }}>
+                전화번호를 입력하십시오
+              </span>
+            )}
+          </div>
           {/* 유저아이디 */}
-          <tr>
-            <th>
-              <label>아이디</label>
-            </th>
-            <td>
-              <input
-                type="text"
-                className="userIdForm"
-                {...register('userId', {
-                  required: '아이디는 필수 항목입니다.',
-                  validate: {
-                    trimValid: (userId: string) => {
-                      if (userId?.trim() === '')
-                        return '아이디를 입력하세요...';
-                      else return true;
-                    },
+          <div className="inputForm">
+            <input
+              type="text"
+              className="userInput userId"
+              {...register('userId', {
+                required: '아이디는 필수 항목입니다.',
+                validate: {
+                  trimValid: (userId: string) => {
+                    if (userId?.trim() === '') return '아이디를 입력하세요...';
+                    else return true;
                   },
-                })}
-                placeholder="아이디"
-              />
-              {errors.userId && (
-                <div style={{ fontSize: '12px', color: 'red' }}>
-                  아이디를 입력하세요
-                </div>
-              )}
-              <button>중복확인</button>
-            </td>
-          </tr>
+                },
+              })}
+              placeholder="아이디"
+            />
+            {errors.userId && (
+              <span style={{ fontSize: '12px', color: 'red' }}>
+                아이디를 입력하세요
+              </span>
+            )}
+          </div>
           {/* 유저 비밀번호 */}
-          <tr>
-            <th>
-              <label>비밀번호</label>
-            </th>
-            <td>
-              <input
-                type="password"
-                className="userPwForm"
-                {...register('userPw', {
-                  required: '비밀번호는 필수 항목입니다.',
-                  validate: {
-                    trimValid: (userPw: string) => {
-                      if (userPw?.trim() === '')
-                        return '비밀번호를 입력하세요....';
-                      else {
-                        return true;
-                      }
-                    },
+          <div className="inputForm">
+            <input
+              type="password"
+              className="userInput userPw"
+              {...register('userPw', {
+                required: '비밀번호는 필수 항목입니다.',
+                validate: {
+                  trimValid: (userPw: string) => {
+                    if (userPw?.trim() === '')
+                      return '비밀번호를 입력하세요....';
+                    else {
+                      return true;
+                    }
                   },
-                  // 비밀번호 유효성 : 8 ~ 10자 영문, 숫자 조합
-                  pattern: /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/,
-                })}
-                placeholder="비밀번호"
-              />
-              {errors.userPw?.message && (
-                <div style={{ fontSize: '12px', color: 'red' }}>
-                  비밀번호를 입력하세요
-                </div>
-              )}
-              {errors.userPw?.type === 'pattern' && (
-                <div style={{ margin: '0', fontSize: '12px', color: 'red' }}>
-                  비밀번호를 형식에 맞춰입력해주세요
-                </div>
-              )}
-            </td>
-          </tr>
-          <button>회원가입</button>
-        </table>
+                },
+                // 비밀번호 유효성 : 8 ~ 10자 영문, 숫자 조합
+                pattern: /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/,
+              })}
+              placeholder="비밀번호"
+            />
+            {errors.userPw?.message && (
+              <span style={{ fontSize: '12px', color: 'red' }}>
+                비밀번호를 입력하세요
+              </span>
+            )}
+            {errors.userPw?.type === 'pattern' && (
+              <span style={{ margin: '0', fontSize: '12px', color: 'red' }}>
+                비밀번호를 형식에 맞춰입력해주세요
+              </span>
+            )}
+          </div>
+          <button className="reigsterBtn">회원가입</button>
+        </div>
       </form>
     </div>
   );
