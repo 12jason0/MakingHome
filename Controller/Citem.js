93ea28347557e442f29d1ad53de05cbb0e52479b
gh9727 <gj9727@gmail.com>
2024-04-03 12:11:08 +0900

    모든 데이터 조회 api

--- Controller/Citem.js
+++ Controller/Citem.js
@@ -1,6 +1,13 @@
 const { Items } = require('../models');
 const { Sequelize, Op } = require('sequelize');
 
+
+// all 모든 상품 페이지 데이터 조회
+const all = async (req, res) => {
+    const all_item = await Items.findAll();
+    console.log('all_item',all_item);
+    res.json({all_item});
+};
 // best 50개 상품 데이터 뽑아오기(현재 20개인데 늘릴거)
 const chart = async (req, res) => {
     const best_item = await Items.findAll({where : {chart : {[Sequelize.Op.between] :  [1,20]}}});
@@ -8,4 +15,4 @@ const chart = async (req, res) => {
     res.json({best_item});
 };
 
-module.exports = { chart };
+module.exports = { all,chart };
