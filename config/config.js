55c38685e42e3b7aeef14ae94632759934747dc4
gh9727 <gj9727@gmail.com>
2024-04-16 11:02:21 +0900

    2

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
-require('dotenv').config(); // 환경변수 사용
+// require('dotenv').config(); // 환경변수 사용
+require('dotenv').config({ path: '.env.production' }); // 환경변수 사용
 module.exports = {
   development: {
     username: process.env.USER,
@@ -16,11 +17,11 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: process.env.production.NAME,
-    password: process.env.production.PASSWORD,
-    database: process.env.production.DATABASE,
-    host: process.env.production.HOST,
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: 'mysql',
-    port: process.env.production.PORT,
+    port: process.env.PORT,
   },
 };
